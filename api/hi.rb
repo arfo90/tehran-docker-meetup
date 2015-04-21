12654fc91cf11e8fb0b7bb7e5babc19bb5d39cff
Amireza <arfo.amireza@gmail.com>
2015-04-21 23:22:26 +0800

    Adding simple endpoint to prove service is working
    This is "Hi" endpoint that return "hi there" when it is called

--- api/hi.rb
+++ api/hi.rb
@@ -1,7 +1,13 @@
 require 'grape'
 
-class Hi < Grape
+class Hi < Grape::API
 
- 
+ version 'v1', using: :header, vendor: 'tehranDocker'
 
+ resource :hi do
+  desc "Saying Hi"
+  get do
+   'Hi there!,,,'
+  end
+ end
 end
---
+++ config.ru
@@ -0,0 +1,4 @@
+#\ -p 80 -o 0
+require_relative 'api/hi'
+
+run Hi
